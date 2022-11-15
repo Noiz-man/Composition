aea81f39393a3b3b32e567018480275bf675c6eb
Noiz-man <denis.ste@mail.ru>
2022-11-15 15:45:28 +0500

    Добавлена реализация DATA слоя

---
+++ app/src/main/java/com/example/composition/data/RepositiryImpl.kt
@@ -0,0 +1,58 @@
+package com.example.composition.data
+
+import com.example.composition.domain.Entity.Level
+import com.example.composition.domain.Entity.Questions
+import com.example.composition.domain.Entity.Settings
+import com.example.composition.domain.Repository.GameRepository
+import com.example.composition.domain.UseCases.GenerateQuestionUseCase
+import com.example.composition.domain.UseCases.GetSettingsUseCase
+import kotlin.random.Random
+
+object RepositiryImpl: GameRepository {
+    override fun getGameSettings(level: Level): Settings {
+        return when(level) {
+            Level.TEST -> Settings(
+                10,
+                4,
+            60,
+                20
+            )
+            Level.EASY -> Settings(
+                20,
+                8,
+            60,
+                30
+            )
+            Level.NORMAL -> Settings(
+                30,
+                10,
+            60,
+                40
+            )
+            Level.HARD -> Settings(
+                100,
+                20,
+            60,
+                60
+            )
+        }
+    }
+
+    private const val MIN_ANSWER_VALUE = 1
+    private const val MIN_SUM_VALUE = 2
+
+
+    override fun generateQuestion(maxSumValue: Int, countOfAnswers: Int): Questions {
+        val sumValue = Random.nextInt(MIN_SUM_VALUE, maxSumValue + 1)
+        val visibleValue = Random.nextInt(MIN_ANSWER_VALUE, sumValue)
+        val options = HashSet<Int>()
+        val rightAnswer = sumValue - visibleValue
+        options.add(rightAnswer)
+        val from = MIN_ANSWER_VALUE
+        val to = maxSumValue - MIN_SUM_VALUE
+        while (options.size < countOfAnswers) {
+            options.add(Random.nextInt(from, to))
+        }
+        return Questions(sumValue, visibleValue, options.toList())
+    }
+}
